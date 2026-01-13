824c7c5996435fabdad856cbd9997a821c4c9032
AnSeongSik-unreal <ahnbs135@naver.com>
2026-01-13 17:56:14 +0900

    Initial commit

---
+++ Source/Project02/Objects/InteractableActor.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Objects/InteractableActor.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AInteractableActor::AInteractableActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM"));
+	StaticMesh->SetSimulatePhysics(false);
+	RootComponent = StaticMesh;
+}
+
+// Called when the game starts or when spawned
+void AInteractableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInteractableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Project02/Objects/InteractableActor.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractableActor.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class PROJECT02_API AInteractableActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+};
